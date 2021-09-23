4bfe70a5ca520d8123b8972cd20717fc0b85bcb5
Graculus-Green <alex.b.blackburn@gmail.com>
2021-09-23 22:07:29 +0100

    Added playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,20 +2,47 @@
 function computerPlay(){
     switch(Math.floor(Math.random()*3 +1)){
         case 1:
-            return 'Rock';
+            return 'rock';
         case 2:
-            return 'Paper'
+            return 'paper'
         case 3:
-            return 'Scissors'
+            return 'scissors'
     }
 }
 
 function playRound(computerSelection, playerSelection){
-    computerSelection = computerPlay();
-    // Need to add a check for valid input
-    playerSelection = window.prompt('Rock, paper or scissors?').toLowerCase();
-  
+    
+    if (computerSelection === playerSelection){
+        return "It's a tie!";
+    }
+    else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            return "You lose! Paper envelopes rock"
+        }
+        else if (computerSelection == 'scissors'){
+            return "You win! Rock smashes scissors."
+        }
+    }
+    else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            return "You win! Paper envelopes rock."
+        }
+        else if (computerSelection == 'scissors'){
+            return "You lose! Scissors cut paper."
+        }
+    }
+    else {
+        if (computerSelection == 'rock') {
+            return "You lose! Rock smashes scissors."
+        }
+        else if (computerSelection == 'paper'){
+            return "You win! Scissors cut paper."
+        }
+    }
     console.log(playerSelection);
 }
-
-playRound();
+computerSelection = computerPlay();
+    // Need to add a check for valid input
+playerSelection = window.prompt('Rock, paper or scissors?').toLowerCase();
+    
+console.log(playRound(computerSelection, playerSelection));
