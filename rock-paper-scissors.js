621f65cd837c7351f7a12ae96321ceedbe363877
Graculus-Green <alex.b.blackburn@gmail.com>
2021-09-23 21:50:58 +0100

    Added in computer selection and user prompt

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,21 @@
-console.log("Hail yourself!")
+
+function computerPlay(){
+    switch(Math.floor(Math.random()*3 +1)){
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper'
+        case 3:
+            return 'Scissors'
+    }
+}
+
+function playRound(computerSelection, playerSelection){
+    computerSelection = computerPlay();
+    // Need to add a check for valid input
+    playerSelection = window.prompt('Rock, paper or scissors?').toLowerCase();
+  
+    console.log(playerSelection);
+}
+
+playRound();
